0d5e19c79b70e26bb656013522ed3c7ffbcb86f5
ShahrzadT <shahrzad.tighnavardmollasaraei@mail.mcgill.ca>
2014-01-25 23:00:46 -0500

    adding the files

---
+++ src/Lab2.java
@@ -0,0 +1,57 @@
+/*
+ * Lab2.java
+ */
+import lejos.nxt.*;
+
+public class Lab2 {
+	public static void main(String[] args) {
+		int buttonChoice;
+
+		// some objects that need to be instantiated
+		Odometer odometer = new Odometer();
+		OdometryDisplay odometryDisplay = new OdometryDisplay(odometer);
+		OdometryCorrection odometryCorrection = new OdometryCorrection(odometer);
+
+		do {
+			// clear the display
+			LCD.clear();
+
+			// ask the user whether the motors should drive in a square or float
+			LCD.drawString("< Left | Right >", 0, 0);
+			LCD.drawString("       |        ", 0, 1);
+			LCD.drawString(" Float | Drive  ", 0, 2);
+			LCD.drawString("motors | in a   ", 0, 3);
+			LCD.drawString("       | square ", 0, 4);
+
+			buttonChoice = Button.waitForAnyPress();
+		} while (buttonChoice != Button.ID_LEFT
+				&& buttonChoice != Button.ID_RIGHT);
+
+		if (buttonChoice == Button.ID_LEFT) {
+			for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { Motor.A, Motor.B, Motor.C }) {
+				motor.forward();
+				motor.flt();
+			}
+
+			// start only the odometer and the odometry display
+			odometer.start();
+			odometryDisplay.start();
+		} else {
+			// start the odometer, the odometry display and (possibly) the
+			// odometry correction
+			odometer.start();
+			odometryDisplay.start();
+			// odometryCorrection.start();
+
+			// spawn a new Thread to avoid SquareDriver.drive() from blocking
+			(new Thread() {
+				public void run() {
+					SquareDriver.drive(Motor.A, Motor.B, 2.85, 2.85, 16.1);
+				}
+			}).start();
+		}
+		
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		System.exit(0);
+	}
+}
---
+++ src/Odometer.java
@@ -0,0 +1,169 @@
+
+import lejos.nxt.*;
+/*
+ * Odometer.java
+ */
+
+public class Odometer extends Thread {
+	// robot position
+	private double x, y, theta;
+
+	 // Other private variables
+    private double rightTachoCount, leftTachoCount;
+    private int previousRightTacho = Motor.B.getTachoCount();
+    private int previousLeftTacho = Motor.A.getTachoCount();
+    final private double wheelRadius = 2.85;
+    final private double width = 16.1;
+    private double rightArcLength, leftArcLength;
+    private double deltaTheta, deltaRobotArcLength;
+    
+    
+	// odometer update period, in ms
+	private static final long ODOMETER_PERIOD = 25;
+
+	// lock object for mutual exclusion
+	private Object lock;
+
+	// default constructor
+	public Odometer() {
+		x = 0.0;
+		y = 0.0;
+		theta = 0.0;
+		lock = new Object();
+	}
+
+	// run method (required for Thread)
+	public void run() {
+		long updateStart, updateEnd;
+
+		while (true) {
+			updateStart = System.currentTimeMillis();
+			// put (some of) your odometer code here
+			
+			 // First, we get the current tacho count for each motor (in radians)
+            rightTachoCount = convertToRadians(Motor.B.getTachoCount()
+                            - previousRightTacho);
+            leftTachoCount = convertToRadians(Motor.A.getTachoCount()
+                            - previousLeftTacho);
+
+            // Once this is done, we set the current tacho as the previous
+            // tacho, which we'll use at the next step
+            previousRightTacho = Motor.B.getTachoCount();
+            previousLeftTacho = Motor.A.getTachoCount();
+
+            // We use a method detailed below to calculate the arc length
+            // traveled by each wheel
+            rightArcLength = calculateArcLength(rightTachoCount, wheelRadius);
+            leftArcLength = calculateArcLength(leftTachoCount, wheelRadius);
+
+            // We calculate both our change in angle and our change in arc
+            // length
+            deltaTheta = (rightArcLength - leftArcLength) / width;
+            deltaRobotArcLength = (rightArcLength + leftArcLength) / 2;
+
+			
+			
+			synchronized (lock) {
+				// don't use the variables x, y, or theta anywhere but here!
+				theta = -0.7376;
+			}
+
+			// this ensures that the odometer only runs once every period
+			updateEnd = System.currentTimeMillis();
+			if (updateEnd - updateStart < ODOMETER_PERIOD) {
+				try {
+					Thread.sleep(ODOMETER_PERIOD - (updateEnd - updateStart));
+				} catch (InterruptedException e) {
+					// there is nothing to be done here because it is not
+					// expected that the odometer will be interrupted by
+					// another thread
+				}
+			}
+		}
+	}
+
+	// accessors
+	public void getPosition(double[] position, boolean[] update) {
+		// ensure that the values don't change while the odometer is running
+		synchronized (lock) {
+			if (update[0])
+				position[0] = x;
+			if (update[1])
+				position[1] = y;
+			if (update[2])
+				position[2] = theta;
+		}
+	}
+
+	public double getX() {
+		double result;
+
+		synchronized (lock) {
+			result = x;
+		}
+
+		return result;
+	}
+
+	public double getY() {
+		double result;
+
+		synchronized (lock) {
+			result = y;
+		}
+
+		return result;
+	}
+
+	public double getTheta() {
+		double result;
+
+		synchronized (lock) {
+			result = theta;
+		}
+
+		return result;
+	}
+
+	// mutators
+	public void setPosition(double[] position, boolean[] update) {
+		// ensure that the values don't change while the odometer is running
+		synchronized (lock) {
+			if (update[0])
+				x = position[0];
+			if (update[1])
+				y = position[1];
+			if (update[2])
+				theta = position[2];
+		}
+	}
+
+	public void setX(double x) {
+		synchronized (lock) {
+			this.x = x;
+		}
+	}
+
+	public void setY(double y) {
+		synchronized (lock) {
+			this.y = y;
+		}
+	}
+
+	public void setTheta(double theta) {
+		synchronized (lock) {
+			this.theta = theta;
+		}
+	}
+	
+	// This method calculates an arc length based on the tacho count and the wheel radius
+    private double calculateArcLength(double tachoCount, double wheelRadius) {
+            return (tachoCount * wheelRadius);
+    }
+
+    // This method converts angles is degrees to angles in radians
+    private double convertToRadians(int tachoCount) {
+            return (tachoCount * 2 * Math.PI) / (360.0);
+    }
+	
+}
